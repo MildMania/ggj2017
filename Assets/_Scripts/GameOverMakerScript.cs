bbe781637886772bb27a7913c40f2756663d5e64
mrvc <o@mildmania.com>
2017-01-21 19:11:34 +0300

    Asset Update

---
+++ Assets/_Scripts/GameOverMakerScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMakerScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 8)
+            GameManager.Instance.GameOver();
+    }
+}
